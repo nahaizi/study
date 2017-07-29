e114bb17b799944c8030cdf22eaa57b34933eda6
lijunyu <371490890@qq.com>
2017-07-29 10:37:49 +0800

    添加lo4j2

--- src/main/java/study/Test.java
+++ src/main/java/study/Test.java
@@ -1,16 +1,18 @@
 package study;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.crypto.hash.Sha256Hash;
 import org.apache.shiro.session.UnknownSessionException;
 import org.apache.shiro.subject.Subject;
 
-public class test {
-
+public class Test {
+    private static Logger log = LogManager.getLogger(Test.class);
 	public static void main(String[] args) {
+		log.debug("-------------------------------begin------------------------");
 		String pwd = new Sha256Hash("123", "SFC256").toString();
-
 		// SHIRO login
 		UsernamePasswordToken token = new UsernamePasswordToken("admin",
 				pwd);
